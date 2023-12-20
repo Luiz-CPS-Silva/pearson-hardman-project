e3fe1fca72303bfaeef9d1f277b1c64916bca717
Luiz Carlos <luizx720@gmail.com>
2023-12-20 16:56:19 -0300

    Update pearson-hardman-project

--- projeto/js/scripts.js
+++ projeto/js/scripts.js
@@ -3,9 +3,11 @@ const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
-// Funções
+// funções
 
 // Eventos
-menuBtn.addEventListener("click", (e) => {
-    menu.classList.add("menu-active");
+[menuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        menu.classList.toggle("menu-active");
+    });
 });
